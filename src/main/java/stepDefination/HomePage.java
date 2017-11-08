57d52ef110c137636f1bb0c338d56fc483dac3b4
Rajat Kumar Sanu <rajatkumar.sanu@varian.com>
2017-11-08 20:33:03 +0530

    Before in StepDefination

--- src/main/java/stepDefination/HomePage.java
+++ src/main/java/stepDefination/HomePage.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 
 import org.openqa.selenium.WebDriver;
 
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import utility.BrowserInteraction;
 import framework.StepDefination;
@@ -15,6 +18,11 @@ public class HomePage extends StepDefination{
 	public HomePage() throws IOException {
 		super();
 		driver = getDriver();
+	}
+	
+	@Before
+	public void setup(Scenario sc){
+		
 	}
 	
 	@Given("^Navigate Main page of book buy india$")
